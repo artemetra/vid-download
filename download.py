61fdda11eda3dc61a7caf4a0eab58e2dcbe5db22
Artem Lukin <artemlukin15@gmail.com>
2021-08-24 16:44:42 +0300

    added ffmpeg_download
    creates a queue from all existing ts videos and downloads from that queue

--- download.py
+++ download.py
@@ -1,12 +1,13 @@
 import requests
 import os
-import time
+import subprocess
+import sys
 
 domain = 'https://cfvod.kaltura.com/hls/p/1674401/sp/167440100/serveFlavor/entryId/1_fmyw2q7s/v/1/ev/4/flavorId/1_0dvpd0pe/name/a.mp4/'
 # segment format: seg-{num}-v1-a1.ts
 dir = "D:\\test\\vid-download\\results\\"
 
-def main() -> None:
+def download_directly() -> None:
     i = 1
     while True:
         result = requests.get(domain + f"seg-{i}-v1-a1.ts")
@@ -19,6 +20,29 @@ def main() -> None:
             print(f"FAIL: Response returned {result.status_code} at index {i}, exiting.")
             break
 
+def ffmpeg_download() -> None:
+    i = 1
+    queue = dir + "queue.txt"
+    if os.path.exists(queue): os.remove(queue)
+    while True:
+        result = requests.get(domain + f"seg-{i}-v1-a1.ts")
+        if result.ok:
+            with open(queue, 'a') as f:
+                f.write(f"file '{result.url}'\n")
+            i += 1
+        else:
+            break
+    
+    # ffmpegres = subprocess.run(f'ffmpeg -f concat -safe 0 -i queue.txt copy output.mp4', 
+    #                 shell = True,
+    #                 stdout=subprocess.PIPE,
+    #                 stderr=subprocess.PIPE)
+    os.system(f'ffmpeg -f concat -safe 0 \
+                -protocol_whitelist file,http,https,tcp,tls,crypto \
+                -i queue.txt \
+                output.mp4')
+
+
 if __name__ == '__main__':
     os.chdir(dir)
-    main()
+    ffmpeg_download()
